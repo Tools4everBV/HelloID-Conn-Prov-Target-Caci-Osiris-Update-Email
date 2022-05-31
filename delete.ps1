2d0aa8f09da230f408b05e7800e1cb14dbfbe23a
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2022-05-31 15:38:54 +0200

    Create delete.ps1

---
+++ delete.ps1
@@ -0,0 +1,129 @@
+##########################################################
+# HelloID-Conn-Prov-Target-Caci-Osiris-Update-Email-Delete
+#
+# Version: 1.0.0
+##########################################################
+$VerbosePreference = "Continue"
+
+# Initialize default value's
+$config = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $AccountReference | ConvertFrom-Json
+$success = $false
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Mapping
+$account = @{
+    studentNummer = $p.ExternalId
+    e_mailadres   = $null
+}
+
+#region functions
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            MyCommand             = $ErrorObject.InvocationInfo.MyCommand
+            RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $httpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $httpErrorObj.ErrorMessage = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+try {
+    # Begin
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Api-Key", $config.ApiKey)
+
+    Write-Verbose "Verify if Caci-Osiris account for: [$($p.DisplayName)] exists"
+    $splatParams = @{
+        Uri     = "$($config.BaseUrl)/basis/student?p_studentnummer=$($aRef)"
+        Method  = 'GET'
+        Headers = $headers
+    }
+    $responseGetUser = Invoke-RestMethod @splatParams
+    
+    $action = 'Delete'
+    $msg = "$action Caci-Osiris eMailAddress: [$($account.e_mailadres)] for: [$($p.DisplayName)] will be executed during enforcement"
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true){
+        $auditLogs.Add([PSCustomObject]@{
+            Message = $msg
+        })
+    }
+
+    if (-not($dryRun -eq $true)) {
+        switch ($action){
+            'Delete' {
+                Write-Verbose "Removing Caci-Osiris eMailAddress for: [$($p.DisplayName)]"
+                $body = @{
+                    p_studentnummer = $account.studentNummer
+                    p_e_mail_adres = $account.e_mailadres
+                } | ConvertTo-Json -Depth 10
+
+                $splatParams = @{
+                    Uri         = "$($config.BaseUrl)/basis/student/update_account"
+                    Method      = 'PUT'
+                    Body        = $body
+                    Headers     = $headers
+                    ContentType = 'application/json'
+                }
+                $responseUpdateUser = Invoke-RestMethod @splatParams
+                if ($responseUpdateUser.statusmeldingen.Count -eq 0){
+                    $accountReference = $responseGetUser.studentnummer
+                    $success = $true
+                    $auditLogs.Add([PSCustomObject]@{
+                        Message = "Removing emailAddress for: [$($p.DisplayName)]"
+                        IsError = $false
+                    })
+                } elseif ($responseUpdateUser.statusmeldingen.Count -gt 0) {
+                    $success = $false
+                    $auditLogs.Add([PSCustomObject]@{
+                        Message = "Could not remove e-mail for student with Error: $($responseUpdateUser.statusmeldingen[0].tekst)"
+                        IsError = $false
+                    })
+                }
+            }
+
+        }
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-HTTPError -ErrorObject $ex
+        $errorMessage = "Could not remove Caci-Osiris eMail account for: [$($p.DisplayName)]. Error: $($errorObj.ErrorMessage)"
+    } else {
+        $errorMessage = "Could not remove Caci-Osiris eMail account for: [$($p.DisplayName)]. Error: $($ex.Exception.Message)"
+    }
+    Write-Verbose $errorMessage
+    $auditLogs.Add([PSCustomObject]@{
+        Message = $errorMessage
+        IsError = $true
+    })
+} finally {
+    $result = [PSCustomObject]@{
+        Success   = $success
+        Account   = $account
+        Auditlogs = $auditLogs
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
